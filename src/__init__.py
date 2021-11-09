e783fce7f7a053bd5fc172a1df4cdb3f0f9d32c3
Xavier Hernandez <xtriminioh@gmail.com>
2021-11-09 12:48:33 -0600

    Upload documento excel .slxs is well done

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,15 @@
 # Manhattan - Proyecto de vista de informacion
 import os
 from flask import Flask
-from flask import render_template
-from flask import request
+from flask import(flash, request, render_template, url_for, redirect)
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = os.path.join(os.getcwd(),'src/files') 
+ALLOWED_EXTENSIONS = {'xlsx'}
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.',1)[1].lower() in ALLOWED_EXTENSIONS
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
@@ -21,12 +28,32 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    @app.route('/dashboard', methods=('GET','POST'))
-    def index():
+    @app.route('/dashboard', methods=['GET','POST'])
+    def dashboard():
         if request.method == 'POST':
             customer_name = request.form['CustomerName']
             print(customer_name)
-        return render_template('index.html')
+        return render_template('search.html')
+
+    @app.route('/upload', methods=['GET','POST'])
+    def upload():
+        if request.method == 'POST':
+            if 'file' not in request.files:
+                flash('No file part')
+                return redirect(request.url)
+            ffile = request.files['file']
+
+            if ffile.filename == '':
+                flash('No selected file')
+                return redirect(request.url)
+
+            if ffile and allowed_file(ffile.filename):
+                filename = secure_filename(ffile.filename)
+                filename = filename.lower()
+                filepath = os.path.join(UPLOAD_FOLDER, filename)
+                ffile.save(filepath)
+
+        return render_template('upload.html')
 
     return app
-    
+
